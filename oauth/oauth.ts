4f523d72b7ea50063664aeb2f53b1fefe9a7d204
anselbrandt <ansel@anselbrandt.com>
2021-06-01 16:55:14 -0400

    Update types

--- oauth/oauth.ts
+++ oauth/oauth.ts
@@ -1,11 +1,11 @@
 import { Providers } from "./types";
-import { Request } from "express";
+import { NextApiRequest } from "next";
 
 export const Oauth = async (providers: Providers) => {
   const getRedirect = async (provider: string) =>
     await providers[provider].getRedirect();
 
-  const getProfile = async (provider: string, req: Request) =>
+  const getProfile = async (provider: string, req: NextApiRequest) =>
     await providers[provider].getProfile(req);
 
   return { getRedirect, getProfile };
